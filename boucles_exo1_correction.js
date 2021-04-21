e2bc2de0cd1f77a7dcddf455ecc4cb4a3559b7a8
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-04-21 14:17:35 +0200

    over

---
+++ boucles_exo1_correction.js
@@ -0,0 +1,13 @@
+//exo1
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+for (let i = 0; i < eleves.length; i++) {
+    console.log("Bonjour " + eleves[i]);
+}
+
+//exo2
+let nombre = parseInt(prompt('Inserez un nombre'));
+
+for (let i = 0; i <= nombre; i++) {
+    console.log(i);
+}
